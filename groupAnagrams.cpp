1d016cedf3c243d3899f3e13a099f84655a1bb4e
the-baron123 <roncohen818@gmail.com>
2025-05-01 18:00:23 +0200

    My First Commit(Group Anagrams leet code)

---
+++ groupAnagrams.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <map>
+#include <algorithm>
+/*slower solution but still more uniqe(in my opinion at least)*/
+std::vector<std::vector<std::string>> groupAnagrams(std::vector<std::string>& strs) {
+	std::vector<std::vector<std::string>> res;
+	std::map<std::map<char, int>, std::vector<std::string>> groups;//hold the groups of anagrams by index to put in the answer after
+	std::map<char, int> MapForWord;
+
+	for (std::string word : strs)
+	{
+		for (char c : word)
+		{
+			MapForWord[c]++;
+		}
+		groups[MapForWord].push_back(word);
+		MapForWord.clear();
+	}
+	for (auto& entry : groups)
+	{
+		res.push_back(std::move(entry.second));
+	}
+	return res;
+}
+
+/*second solution faster and more simple but still not optimal I could'nt understand the idea :( */
+std::vector<std::vector<std::string>> groupAnagrams(std::vector<std::string>& strs)
+{
+	std::unordered_map<std::string, std::vector<std::string>> res;
+	for (const auto& s : strs) {
+		std::string sortedS = s;
+		sort(sortedS.begin(), sortedS.end());
+		res[sortedS].push_back(s);
+	}
+	std::vector<std::vector<std::string>> result;
+	for (auto& pair : res) {
+		result.push_back(pair.second);
+	}
+	return result;
+}
+int main()
+{
+
+}
