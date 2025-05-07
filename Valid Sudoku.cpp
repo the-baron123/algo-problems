6f0d47b02e98a340c94fa4641a5b22163ca33138
the-baron123 <roncohen818@gmail.com>
2025-05-07 04:50:26 +0300

    Now its 3

---
+++ Valid Sudoku.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+
+/*
+main idea is to build side functions to look-up each row and colom
+and one to search on the qube that we are currently at
+*/
+bool isValidSudoku(vector<vector<char>>& board)
+{
+
+}
+
+int main()
+{
+
+}
